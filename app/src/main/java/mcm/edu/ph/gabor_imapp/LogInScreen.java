10f7cade3a5fc320a8c95c6b1afc96c2766b59a5
Annie Gabor <kaidengabs@gmail.com>
2021-01-25 17:34:08 +0800

    GABOR_IMApp

---
+++ app/src/main/java/mcm/edu/ph/gabor_imapp/LogInScreen.java
@@ -0,0 +1,14 @@
+package mcm.edu.ph.gabor_imapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogInScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in_screen);
+    }
+}
